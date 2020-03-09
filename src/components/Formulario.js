6f92e62df43bde5d0475886e3ca3ced407a356a2
Alberto Valdés <albertoavv@hotmail.es>
2020-03-09 12:30:46 -0400

    Formulario
    Hooks del formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import styles from './Formulario.module.css';
+import useSelect from '../hooks/useSelect';
 
 const Formulario = () => {
+
+    const OPCIONES = [
+        {value: 'general', label: 'General'},
+        {value: 'business', label: 'Negocios'},
+        {value: 'entertainment', label: 'Entretenimiento'},
+        {value: 'health', label: 'Salud'},
+        {value: 'science', label: 'Ciencia'},
+        {value: 'sports', label: 'Deportes'},
+        {value: 'technology', label: 'Tecnología'},
+
+    ]
+
+    //utilizar custom hook
+    const [categoria, SelectNoticias]  = useSelect('science', OPCIONES);
+
     return (
-        <div className="row">
+        <div className={`${styles.buscardor} row`}>
             <div className="col s12 m8 offset-m2">
                 <form>
                     <h2 className={styles.heading}>Encuentra Noticias por categorias</h2>
 
+                        <SelectNoticias />
+
                     <div className="input-field col s12">
-                        <input 
+                        <input
                             type="submit"
-                            className="btn-large amber darken-2"
+                            className={`${styles.btn_block} btn-large amber darken-2`}
                             value="Buscar"
                         />
                     </div>
